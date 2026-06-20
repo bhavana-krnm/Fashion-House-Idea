e9417be4bcdca478d5fb10f4c02a3b7bb7e9459f
bhavana-krnm <bhavanakrnm@gmail.com>
2026-06-20 09:41:21 +0530

    mini-project

--- web-site/src/App.jsx
+++ web-site/src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-
 import { CartProvider } from "./CartContext";
 
 import IndexPage from "./IndexPage";
@@ -9,27 +8,37 @@ import ProductDetailPage from "./ProductDetailPage";
 
 import CartPage from "./CartPage";
 import BuyNowPage from "./BuyNowPage";
+import CheckoutPage from "./CheckoutPage";
+
 import OrderConfirmPage from "./OrderConfirmPage";
 import OrderTrackingPage from "./OrderTrackingPage";
 
 import Signup from "./Signup";
+import Signin from "./Signin";
 import Profile from "./Profile";
 import Wishlist from "./Wishlist";
-import CheckoutPage from "./CheckoutPage";
 
 export default function App() {
   return (
     <CartProvider>
       <Routes>
+        {/* Home */}
         <Route path="/" element={<IndexPage />} />
 
+        {/* Authentication */}
         <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+
+        {/* User */}
         <Route path="/profile" element={<Profile />} />
         <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/cart" element={<CartPage />} />
-<Route path="/checkout" element={<CheckoutPage />} />
-<Route path="/buy-now" element={<BuyNowPage />} />
 
+        {/* Shopping */}
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/buy-now" element={<BuyNowPage />} />
+
+        {/* Categories & Products */}
         <Route path="/categories" element={<CategoryPage />} />
         <Route path="/products/:category" element={<ProductPage />} />
         <Route
@@ -37,10 +46,7 @@ export default function App() {
           element={<ProductDetailPage />}
         />
 
-        <Route path="/cart" element={<CartPage />} />
-
-        <Route path="/buy-now" element={<BuyNowPage />} />
-
+        {/* Orders */}
         <Route path="/confirm" element={<OrderConfirmPage />} />
         <Route path="/tracking" element={<OrderTrackingPage />} />
       </Routes>
